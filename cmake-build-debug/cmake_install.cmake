060a1db404af0a046325aed90ffb99e0730b46b5
BhavinRaichura <bhavinraichura28@gmail.com>
2022-10-13 11:39:42 +0530

    first

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/bhavin/CLionProjects/untitled1
+# Install script for directory: /home/bhavin/CLionProjects/pong-ball-game
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/bhavin/CLionProjects/untitled1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/bhavin/CLionProjects/pong-ball-game/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -5,47 +5,65 @@
 int main()
 {
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My First Game");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Pong Ball");
     //window.setVerticalSyncEnabled(true);
 
     // Fram-rate
     window.setFramerateLimit(120);
 
-    // create object
+    ///////////////////
+    //////////////
+    // Create object
+
     // handler obj1
     sf::RectangleShape obj1(sf::Vector2f(20,100));
     obj1.setFillColor(sf::Color::Blue);
     obj1.setOrigin(10,100);
     obj1.setPosition(20,200);
 
-
     // boll obj2
     sf::CircleShape obj2(10.f);
     obj2.setFillColor(sf::Color::Red);
     obj2.setPosition(400,300);
 
+    // Initial moving position
     int yPosPad = 0;
     int xPosBall = -4;
     int yPosBall = -2;
 
+    ///////////////
+    ///////////
+    // Sounds
 
-    std:: cout << "Game Started" <<std::endl;
-
-    // Sound
+    // Ball Bounce sound
     sf::SoundBuffer ball_buffer;
     ball_buffer.loadFromFile("project-data/sound/bounce.wav");
-
     sf::Sound ball_bounce;
     ball_bounce.setBuffer(ball_buffer);
 
+    // Wall touch sound
+    sf::SoundBuffer wall_sound_buffer;
+    wall_sound_buffer.loadFromFile("project-data/sound/wall_bump.wav");
+    sf::Sound wall_touch;
+    wall_touch.setBuffer(wall_sound_buffer);
+
+    ///////////////
+    //////////
+
+    std::cout<<"\nGame Started\n";
+
     while(window.isOpen()){
 
+        /////////////////
+        ///////////
         //event part on the main game loop
         sf::Event event;
 
         while(window.pollEvent(event)) {
 
+            //////////
             // process events
+
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
@@ -79,32 +97,42 @@ int main()
 
         }
 
-        // logic  handling
+        ///////////////////////
+        //////////////
+        // Logic Handling
+
         // Ball logic
         obj2.move(xPosBall,yPosBall);
 
         // out of boundary chech
         if(obj2.getPosition().y < 0){
             yPosBall = -yPosBall;
+            wall_touch.play();
         }
         if(obj2.getPosition().y > 600){
             yPosBall = -yPosBall;
+            wall_touch.play();
         }
         if(obj2.getPosition().x > 800){
             xPosBall = -xPosBall;
+            wall_touch.play();
         }
 
-
+        //////////////////////
+        ////////////
         // movement
+
         if(obj2.getGlobalBounds().intersects(obj1.getGlobalBounds())==true)
         {
             xPosBall= -xPosBall;
             ball_bounce.play();
         }
 
+
+        //////////////////////
+        /////////////////
         // rendering part of the main game loop
         window.clear();
-
 
         // draw things
         window.draw(obj1);
